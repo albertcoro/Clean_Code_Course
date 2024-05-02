473bcd5ce60e70de046e801c46860b5db9a55796
Albert Corominas <acorominas@nexusgeographics.com>
2024-05-02 13:42:48 +0200

    New Clean

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -5,39 +5,73 @@
         name: string;
     }
     
+    class Mailer{
+        private masteremail: string = 'test@test.com';
+
+        sendEmail( emailList: string[], template:'to-clients'|'to-admin', message: string ) {
+            console.log('Enviando correo a los clientes', emailList, message );
+        }
+    }
+
+    class ProductService{
+
+        getProduct( id: number ) {
+            console.log('Producto: ',{ id, name: 'OLED Tv' });
+        }
+
+        saveProduct( product: Product ) {
+            console.log('Guardando en base de datos', product );
+        }
+    }
     // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
     // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
     class ProductBloc {
+
+        private productService: ProductService;
+        private mailer: Mailer;
+
+        constructor(productService: ProductService, mailer: Mailer) {
+            this.productService = productService;
+            this.mailer = mailer;
+        }
     
         loadProduct( id: number ) {
             // Realiza un proceso para obtener el producto y retornarlo
-            console.log('Producto: ',{ id, name: 'OLED Tv' });
+            this.productService.getProduct(id);
         }
     
         saveProduct( product: Product ) {
-            // Realiza una petición para salvar en base de datos 
-            console.log('Guardando en base de datos', product );
+            this.productService.saveProduct(product);
         }
     
         notifyClients() {
-            console.log('Enviando correo a los clientes');
+            this.mailer.sendEmail(['test@test.com'], 'to-clients', 'Nuevo producto en la tienda');
         }
     
-        onAddToCart( productId: number ) {
-            // Agregar al carrito de compras
-            console.log('Agregando al carrito ', productId );
-        }
+        
     
     }
+
+    class CartBloc {
+
+        private items: Product[] = [];
+
+        onAddToCart( product: Product ) {
+            // Agregar al carrito de compras
+            console.log('Agregando al carrito ', product );
+        }
+    }
     
+    const productService = new ProductService();
+    const mailer = new Mailer();
 
-
-    const productBloc = new ProductBloc();
+    const productBloc = new ProductBloc(productService, mailer);
+    const cartBloc    = new CartBloc();
 
     productBloc.loadProduct(10);
     productBloc.saveProduct({ id: 10, name: 'OLED TV' });
     productBloc.notifyClients();
-    productBloc.onAddToCart(10);
+    cartBloc.onAddToCart({ id: 10, name: 'OLED TV' });
 
 
 
