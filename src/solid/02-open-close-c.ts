434192a9a67f48e7d5f19a53c8f8ed89e83332fe
Albert Corominas <acorominas@nexusgeographics.com>
2024-05-06 08:41:48 +0200

    New Changes

--- src/solid/02-open-close-c.ts
+++ src/solid/02-open-close-c.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
-
 export class HttpClient {
     async get(url: string) {
-        const { data, status } = await axios.get(url);
+        const resp = await fetch(url);
+        const data = await resp.json();
+        const status = resp.status;
         return { data, status };
     }
 }
